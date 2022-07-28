09cc14a2be80137bee928d2754e4d45e094a1f42
sheetal soni <sheetal.soni@hotmail.com>
2022-07-28 15:10:37 -0500

    fontawesome fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
