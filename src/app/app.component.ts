208ae27ba913957bf94f1feb292526e3d901dc33
sheetal soni <sheetal.soni@hotmail.com>
2022-07-28 16:18:32 -0500

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'CodingCrew_2022';
