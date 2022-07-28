6f309f6a8e0d117ec1cfa3d446d922a4b2f27e2c
sheetal soni <sheetal.soni@hotmail.com>
2022-07-28 17:41:24 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MarketComponent } from './market/market.component';
+import { RoutesComponent } from './routes/routes.component';
+import { SelectionComponent } from './selection/selection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'market', component:  MarketComponent},
+  { path: 'routes', component:  RoutesComponent},
+  { path: 'selection', component:  SelectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoutesComponent } from './routes/routes.component';
+import { MarketComponent } from './market/market.component';
+import { SelectionComponent } from './selection/selection.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    RoutesComponent,
+    MarketComponent,
+    SelectionComponent,
   ],
   imports: [
     BrowserModule,
